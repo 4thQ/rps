fd51a8df04f0d2642e30c1242a6681ae15ac8112
Nirajan Gurung <diorrg@outlook.com>
2024-10-29 20:33:38 +0000

    kind of not stuck

--- rps.js
+++ rps.js
@@ -1,66 +1,73 @@
 let humanScore = 0;
 let computerScore = 0;
+let userInput = "";
 
 function getCompMove() {
-  let rps = ["rock", "paper", "scissors"];
+  let rps = ["rock", "paper", "scissor"];
 
   let randomNum = Math.floor(Math.random() * rps.length);
 
   return rps[randomNum];
 }
 
-let userInput = "sdf";
-
 let weps = document.querySelector(".weapons");
 
 weps.addEventListener("click", (e) => {
-  if (e.target.value == "rock") {
-    userInput = e.target.value;
-    console.log("User selected:", userInput);
+  const result = document.querySelector(".result");
+  if (result.hasChildNodes()) {
+    result.removeChild(result.firstChild);
+
+    if (e.target.value == "rock") {
+      userInput = e.target.value;
+    }
+
+    compareMoves();
   } else if (e.target.value == "paper") {
     userInput = e.target.value;
-    console.log("User selected:", userInput);
+    compareMoves();
   } else if (e.target.value == "scissor") {
     userInput = e.target.value;
-    console.log("User selected:", userInput);
+    compareMoves();
   }
 });
 
 function compareMoves() {
   let userMove = userInput;
   let compMove = getCompMove();
-  console.log(userMove);
-  console.log(compMove);
+
   if (
     (userMove == "rock" && compMove == "rock") ||
     (userMove == "paper" && compMove == "paper") ||
     (userMove == "scissor" && compMove == "scissor")
   ) {
-    console.log("Its a tie! You chose the same weapon.");
+    const result = document.querySelector(".result");
+    const p = document.createElement("p");
+    p.textContent = "You both picked the same weapon. Its a Tie!";
+    result.appendChild(p);
   } else if (
     (userMove == "rock" && compMove == "scissor") ||
     (userMove == "paper" && compMove == "rock") ||
     (userMove == "scissor" && compMove == "paper")
   ) {
-    console.log("You won! Your weapon is superior.");
+    const result = document.querySelector(".result");
+    const p = document.createElement("p");
+    p.textContent = "Your weapon is stronger. Winner!";
+    result.appendChild(p);
     humanScore++;
   } else if (
     (userMove == "rock" && compMove == "paper") ||
     (userMove == "paper" && compMove == "scissor") ||
     (userMove == "scissor" && compMove == "rock")
   ) {
-    console.log("You lost! Your weapon is weak.");
+    const result = document.querySelector(".result");
+    const p = document.createElement("p");
+    p.textContent = "Loser!";
+    result.appendChild(p);
+    console.log("Your weapon is weak. You lost!");
     computerScore++;
   } else {
-    console.log("Invalid Input");
   }
 }
-
-function playGame() {
-  compareMoves();
-}
-
-playGame();
 
 // if (humanScore > computerScore) {
 //   console.log(
